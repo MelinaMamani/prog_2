90968abb0f5eb4787f6f8b294e8412fdc5835c32
alumno <alumno>
2018-08-21 12:14:05 -0300

    Clase 21/08

---
+++ clase 1/Mamani.Erika.2A/Ejercicio00/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+
+            Console.WriteLine("Hola mundo");
+
+            Console.WriteLine("Ingrese su nombre: ");
+
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su nombre: ");
+
+            edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Su nombre {0} y edad {1} ",nombre, edad);
+
+            Console.ReadLine();
+        }
+    }
+}
