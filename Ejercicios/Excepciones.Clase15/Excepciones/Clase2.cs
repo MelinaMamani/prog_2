f478666df7454d49ed04c7ffefe3ce3048008bca
alumno <alumno>
2018-10-17 12:07:39 -0300

    Clase 15

---
+++ Ejercicios/Excepciones.Clase15/Excepciones/Clase2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class Clase2
+    {
+        public Clase2()
+        {
+            try
+            {
+                Clase.LanzarExcepcion();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            
+        }
+    }
+}
---
+++ Ejercicios/Excepciones.Clase15/Excepciones/UnaExcepcion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class UnaExcepcion : Exception
+    {
+        public UnaExcepcion(string mensaje, Exception innerException) 
+        : base (mensaje, innerException)
+        {
+            throw new Exception(mensaje, i
+        }
+    }
+}
