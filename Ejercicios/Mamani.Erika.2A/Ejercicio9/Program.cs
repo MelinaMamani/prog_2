652bfec6827423b0923235b60e424fb3360504b8
MelinaMamani <erikamelinamamani@gmail.com>
2018-11-12 08:39:10 -0300

    nada

---
+++ Ejercicios/Mamani.Erika.2A/Ejercicio9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "**";
+            string cad = "*";
+
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(cad);
+                cad += cadena;
+            }
+
+            //Console.Write(cad);
+
+            Console.ReadKey();
+        }
+    }
+}
